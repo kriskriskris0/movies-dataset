11d3726207363cae02070c05265eca33b8c1b78d
Кристина <104250279+kriskriskris0@users.noreply.github.com>
2024-10-18 13:30:11 +0000

    mmmm

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,11 @@ import pandas as pd
 import streamlit as st
 
 # Show the page title and description.
-st.set_page_config(page_title="Movies dataset", page_icon="🎬")
-st.title("🎬 Movies dataset")
+st.set_page_config(page_title="Titanic dataset", page_icon="🎬")
+st.title("🎬 Titanic dataset")
 st.write(
     """
-    This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
+    This app visualizes data from [The Titanic Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
     It shows which movie genre performed best at the box office over the years. Just 
     click on the widgets below to explore!
     """
@@ -18,7 +18,7 @@ st.write(
 # reruns (e.g. if the user interacts with the widgets).
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/movies_genres_summary.csv")
+    df = pd.read_csv("data/titanic_train.csv")
     return df
 
 
